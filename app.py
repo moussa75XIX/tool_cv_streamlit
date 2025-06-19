984bc77ef2e2977e82cae916bd99a997154fdbe5
moussa75XIX <59694683+moussa75XIX@users.noreply.github.com>
2025-06-19 09:07:22 +0200

    fix download button

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ st.title("🧠 Générateur Automatique de CV Word")
 uploaded_file = st.file_uploader("Upload ton CV (PDF)", type=["pdf"])
 
 if uploaded_file:
-    with st.spinner("Traitement en cours..."):
-        output_bytes = mapping_cv(file=uploaded_file)
+    if "output_bytes" not in st.session_state:
+        with st.spinner("Traitement en cours..."):
+            output = mapping_cv(file=uploaded_file)
+            if output:
+                st.session_state.output_bytes = output
+                st.success("✅ CV généré avec succès !")
+            else:
+                st.error("Une erreur est survenue lors du traitement.")
 
-        if output_bytes:
-            st.success("✅ CV généré avec succès !")
-            st.download_button(
-                label="📥 Télécharger le CV Word",
-                data=output_bytes,
-                file_name="cv_final.docx"
-            )
-        else:
-            st.error("Une erreur est survenue lors du traitement.")
+    if "output_bytes" in st.session_state:
+        st.download_button(
+            label="📥 Télécharger le CV Word",
+            data=st.session_state.output_bytes,
+            file_name="cv_final.docx"
+        )
